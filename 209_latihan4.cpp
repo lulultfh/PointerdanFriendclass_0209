d2e724a7b6f18220ff7dd013c3837ec55c054e07
lulultfh <punyalulu05@gmail.com>
2024-05-07 15:36:12 +0700

    add class siswa beserta member

--- 209_latihan4.cpp
+++ 209_latihan4.cpp
@@ -11,3 +11,13 @@ class orang
     void setNama(string pNama);
     friend class siswa;
 };
+
+class siswa
+{
+    private:
+    int id;
+
+    public:
+    void setId(int pId);
+    void displayAll(orang &a);
+};
